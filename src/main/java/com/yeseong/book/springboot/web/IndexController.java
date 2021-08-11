7863977b97ce360a383254bcef97fca45069c3bf
Yeseong31 <yeseong31@naver.com>
2021-08-11 11:37:03 +0900

    21/08/11

--- src/main/java/com/yeseong/book/springboot/web/IndexController.java
+++ src/main/java/com/yeseong/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.yeseong.book.springboot.web;
 
+import com.yeseong.book.springboot.config.auth.dto.SessionUser;
 import com.yeseong.book.springboot.service.posts.PostsService;
 import com.yeseong.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +9,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
 /*
     @GetMapping("/")
@@ -20,9 +24,22 @@ public class IndexController {
         return "index";
     }
 */
+/*
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+*/
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
