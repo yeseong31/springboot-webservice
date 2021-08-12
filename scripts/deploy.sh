161d48cf190c346d770bc51c45aa5c4d39d049f2
Yeseong31 <yeseong31@naver.com>
2021-08-12 11:24:19 +0900

    21/08/12

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl springboot-webservice | grep jar | awk '{pring $1}')    # (1)
+CURRENT_PID=$(pgrep -fl springboot-webservice | grep java | awk '{pring $1}')    # (1)
 
 echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
