03dc17035093d9634d5fdbaa47701ed587c38308
Yeseong31 <yeseong31@naver.com>
2021-08-12 17:03:22 +0900

    21/08/12

---
+++ scripts/switch.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# 엔진엑스가 바라보는 스프링 부트를 최신 버전으로 변경
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+function switch_proxy() {
+    IDLE_PORT=$(find_idle_port)
+
+    echo "> 전환할 Port: $IDLE_PORT"
+    echo "> Port 전환"
+    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+
+    echo "> 엔진엑스 Reload"
+    sudo service nginx reload
+}
