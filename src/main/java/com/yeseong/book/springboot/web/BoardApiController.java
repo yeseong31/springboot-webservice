b10cb4987882b11489a901123e8c321ad43ed574
Yeseong31 <yeseong31@naver.com>
2021-08-13 11:59:07 +0900

    21/08/13

---
+++ src/main/java/com/yeseong/book/springboot/web/BoardApiController.java
@@ -0,0 +1,12 @@
+package com.yeseong.book.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class BoardApiController {
+
+
+
+}
